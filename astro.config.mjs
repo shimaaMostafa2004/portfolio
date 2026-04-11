5a4be32740e74123af754ecbcec74a7531ae4efb
abdo <abdotaher2025@gmail.com>
2026-04-11 19:49:53 +0200

    Setup GitHub Actions deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,4 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://abdotaher.me',
-  output: 'static',
-  trailingSlash: 'always',
-  build: {
-    format: 'directory'
-  }
 });
